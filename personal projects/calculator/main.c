4964aabd799cfc795eb3b62a0c915c339ee5c53e
Dilpreet2004 <109076965+Dilpreet2004@users.noreply.github.com>
2024-07-12 12:13:01 +0530

    Add files via upload

---
+++ personal projects/calculator/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float val1,val2;
+    char op;
+    printf("enter the first number :");
+    scanf("%f",&val1);
+    printf("enter the operator :");
+    scanf(" %c",&op);
+    printf("enter the second number :");
+    scanf("%f",&val2);
+
+    switch(op){
+        case '+':
+            printf("%f",val1+val2);
+            break;
+        case '-':
+            printf("%f",val1-val2);
+            break;
+        case '*':
+            printf("%f",val1*val2);
+            break;
+        case '/':
+            if(val2 == 0){
+                printf("error! cannot divide by zero\n");
+            }
+            else{
+            printf("%f",val1/val2);
+            }
+            break;
+        default:
+            printf("invalid operator");
+            break;
+    }
+    return 0;
+}
