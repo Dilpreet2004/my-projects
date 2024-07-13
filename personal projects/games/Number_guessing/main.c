6ca51f819db5b7e8ed138581a637f96be060ada9
Dilpreet2004 <109076965+Dilpreet2004@users.noreply.github.com>
2024-07-13 07:42:57 +0530

    Update main.c

--- personal projects/games/Number_guessing/main.c
+++ personal projects/games/Number_guessing/main.c
@@ -3,7 +3,8 @@
 #include <time.h>
 
 int main()
-{
+{ 
+    //to allow user to select range in which bot should pick number
     int low = 0;
     int high = 0;
     printf("before starting the game, please enter the lower and upper limit within which i have to choose number\n");
@@ -17,12 +18,19 @@ int main()
     int max_guess = 5;
     int guess = 0;
     system("clear");
+
+    //starting game
     printf("this is a number guessing game\n");
     printf("I have chosen number between %d - %d and you have to guess it\n",low,high);
+    
+    //this loop runs until user input correct guess 
     for(int i = max_guess;i > 0;i--){
         printf("you have %d tr%s left\n",i,i == 1?"y":"ies");
+        //to input guess of user
         printf("enter your guess: ");
         scanf("%d",&guess);
+
+        //conditional statements for checking user inputted number
         if(guess == random_number)
         {
             printf("\ncongrats! you have guessed it right\n");
@@ -39,6 +47,7 @@ int main()
             printf("\nYour guess is wrong. My number is greater than yours\n");
         }
     }
+    //following output display only if user failed to guess number in 5 tries
     printf("\nyou are out of moves\n");
     printf("correct number is %d\n",random_number);
     return 0;
