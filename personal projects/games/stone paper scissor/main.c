34cc9f3658457dc4838196d861def16cbc6460a8
Dilpreet2004 <109076965+Dilpreet2004@users.noreply.github.com>
2024-09-02 16:52:32 +0530

    Update main.c

--- personal projects/games/stone paper scissor/main.c
+++ personal projects/games/stone paper scissor/main.c
@@ -1,67 +1,98 @@
+// C program for the above approach
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-//1 for player wins,-1 for computer wins and 0 for tie
-
-char bot,player;
-
-//to check for winner
-int checkForWinner(char bot,char player)
+// Function to implement the game
+int game(char you, char computer)
 {
-    if(bot == player)
-        return 0;
+	// If both the user and computer
+	// has choose the same thing
+	if (you == computer)
+		return -1;
 
-    if(bot == 'r' && player == 'p')
-        return 1;
-    else if(bot == 'p' && player == 'r')
-        return -1;
+	// If user's choice is stone and
+	// computer's choice is paper
+	if (you == 's' && computer == 'p')
+		return 0;
 
-    if(bot == 's' && player == 'r')
-        return 1;
-    else if(bot == 'r' && player == 's')
-        return -1;
+			// If user's choice is paper and
+			// computer's choice is stone
+			else if (you == 'p' && computer == 's') return 1;
 
-    if(bot == 'p'&& player == 's')
-        return 1;
-    else if(bot == 's' && player == 'p')
-        return -1;
+	// If user's choice is stone and
+	// computer's choice is scissor
+	if (you == 's' && computer == 'z')
+		return 1;
+
+	// If user's choice is scissor and
+	// computer's choice is stone
+	else if (you == 'z' && computer == 's')
+		return 0;
+
+	// If user's choice is paper and
+	// computer's choice is scissor
+	if (you == 'p' && computer == 'z')
+		return 0;
+
+	// If user's choice is scissor and
+	// computer's choice is paper
+	else if (you == 'z' && computer == 'p')
+		return 1;
 }
 
+// Driver Code
 int main()
 {
-    //main input screen
-    int result;
-    do{
-    printf("Enter r for ROCK, p for PAPER and s for SCISSOR\n");
-    printf("Player : ");
-    scanf(" %c",&player);
+	// Stores the random number
+	int n;
 
-    //to generate computer choice
-    srand(time(0));
-    int random = rand()%100;
+	char you, computer, result;
 
-    if(random < 33)
-        bot = 'r';
-    else if(random > 33 && random < 66)
-        bot = 'p';
-    else if(random > 66)
-        bot = 's';
+	// Chooses the random number
+	// every time
+	srand(time(NULL));
 
-    //to display winner of game
-    result = checkForWinner(player,bot);
-    if(result == 1){
-        printf("Game Draw!\n");
-    }
-    else if(result == -1){
-        printf("Wow! You have won the game!\n");
-    }
-    else{
-        printf("Game Draw!\n");
-    }
+	// Make the random number less
+	// than 100, divided it by 100
+	n = rand() % 100;
 
-    printf("You choose : %c and Computer choose : %c\n",player, bot);
-    printf("\n\n");
-    }while(result == 0);
-    return 0;
+	// Using simple probability 100 is
+	// roughly divided among stone,
+	// paper, and scissor
+	if (n < 33)
+
+		// s is denoting Stone
+		computer = 's';
+
+	else if (n > 33 && n < 66)
+
+		// p is denoting Paper
+		computer = 'p';
+
+	// z is denoting Scissor
+	else
+		computer = 'z';
+
+	printf("\n\n\n\n\t\t\t\tEnter s for STONE, p for PAPER and z for SCISSOR\n\t\t\t\t\t\t\t");
+
+	// input from the user
+	scanf("%c", &you);
+
+	// Function Call to play the game
+	result = game(you, computer);
+
+	if (result == -1) {
+		printf("\n\n\t\t\t\tGame Draw!\n");
+	}
+	else if (result == 1) {
+		printf("\n\n\t\t\t\tWow! You have won the game!\n");
+	}
+	else { 
+		printf("\n\n\t\t\t\tOh! You have lost the game!\n");
+	}
+		printf("\t\t\t\tYOu choose : %c and Computer choose : %c\n",you, computer);
+
+	return 0;
 }
